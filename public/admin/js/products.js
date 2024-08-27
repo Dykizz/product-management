be77863f1cac121bc99c9c40e957767717276f9e
Dykizz <phungphutai2005@gmail.com>
2024-08-27 23:37:26 +0700

    update

--- public/admin/js/products.js
+++ public/admin/js/products.js
@@ -1,6 +1,6 @@
 const formChangeStatus = document.querySelector("#formChangeStatus");
 const path = formChangeStatus.getAttribute("data-path");
-
+// Change Status
 const buttonsChangeStatus = document.querySelectorAll("[buttonChangeStatus]");
 if (buttonsChangeStatus.length > 0){
     buttonsChangeStatus.forEach(button =>{
@@ -14,3 +14,21 @@ if (buttonsChangeStatus.length > 0){
         })
     })
 }
+// End Change Status
+
+// Delete Item
+const formDeleteItem = document.querySelector("#formDeleteItem");
+const buttonDeletes = document.querySelectorAll("[button-delete-item]");
+if (buttonDeletes.length > 0){
+    buttonDeletes.forEach(button => {
+        button.addEventListener("click", ()=>{
+            const id = button.getAttribute("data-id");
+            const path = formDeleteItem.getAttribute("data-path");
+            const action = `${path}/${id}?_method=DELETE`;
+            formDeleteItem.action = action;
+            formDeleteItem.submit();
+        })
+    })
+}
+
+// End Delete Item
