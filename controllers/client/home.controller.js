2c9123001966a7d46e23d3adb12778d6bd6bc556
Dykizz <phungphutai2005@gmail.com>
2024-09-16 23:39:24 +0700

    Tạo giao diện sản phẩm nổi bật bên trang chủ client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,9 +1,11 @@
 const Categories = require('../../models/categories.model');
-
+const Product = require('../../models/product.model');
 
 // [GET] /
-module.exports.index = (req,res) => {
+module.exports.index = async (req,res) => {
+    const featuredProducts = await Product.find({ deleted : false , featured : true , status : "active"});
     res.render('client/pages/home/index',{
-        pageTitle : "Trang chủ"
+        pageTitle : "Trang chủ",
+        products : featuredProducts
     })
 }
