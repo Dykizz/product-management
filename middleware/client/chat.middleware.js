0cae48ba0998ae0d972966f318bb9a0fe3de752b
Dykizz <phungphutai2005@gmail.com>
2024-10-20 20:45:54 +0700

    Thay đổi nhỏ

--- middleware/client/chat.middleware.js
+++ middleware/client/chat.middleware.js
@@ -1,11 +1,25 @@
 const RoomChat = require('../../models/room-chat.model');
-module.exports = async (req,res,next) => {
+
+module.exports = async (req, res, next) => {
     const room_chat_id = req.params.id;
+    const user_id = res.locals.account.id;
+
     try {
-        const existRoom = await RoomChat.findOne({_id : room_chat_id });
-        if (existRoom) next();
-        else res.redirect('/');
+        // Truy vấn kiểm tra xem có phòng không và user có trong danh sách không
+        const room = await RoomChat.findOne({
+            _id: room_chat_id,
+            deleted: false,
+            'users.user_id': user_id  // Kiểm tra trong mảng users có user_id mong muốn
+        });
+
+        if (room) {
+            next();  // Nếu phòng tồn tại và user có trong phòng, tiếp tục
+        } else {
+            res.redirect('/');  // Nếu không, redirect về trang chủ
+        }
+
     } catch (error) {
+        console.error(`Error fetching room chat: ${error.message}`);
         res.redirect('/');
     }
-}
+};
